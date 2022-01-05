ebf70686a8bb5ed820beef7f8fb54c89c9b02a27
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-05 11:51:01 +0530

    first user data request sent  successfully

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
 router.post('/', (req, res) => {
-    res.send('Resister a user');
+    res.send(req.body);
 });
 
 module.exports = router;
