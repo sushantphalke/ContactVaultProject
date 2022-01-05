cffcc81ed64934b4888c79a68b225160137857a7
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-05 10:05:54 +0530

    express server setup with initial routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
 const app = express();
-app.get('/',(req,res)=>res.json({msg:'Welcome to ContactVault API'}));
-app.use('/api/users',require('./route/users'));
+
+app.get('/', (req, res) =>
+    res.json({ msg: 'Welcome to ContactVault API', Author: 'Sushant Phalke' })
+);
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
