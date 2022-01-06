da670176bd58cec63bd29cb973c13ef38767f753
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-06 10:48:36 +0530

    auth js for middleware is added to auth js of routes

--- routes/auth.js
+++ routes/auth.js
@@ -12,8 +12,15 @@ const User = require('../models/User');
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Privet
-router.get('/', auth, (req, res) => {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    // res.send('Get logged in user');
+   try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server Error');
+   }
 });
 
 // @route   POST api/auth
