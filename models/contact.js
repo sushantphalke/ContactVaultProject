133c7d4ba10fcb60c96fdd22a664d89ff5f14d44
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-06 16:38:46 +0530

    getting users contacts successfully

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const UserSchema = mongoose.Schema({
+const ContactSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users',
