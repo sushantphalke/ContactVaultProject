89ff81d52e4f5e8372ddd5aac703244afc370b17
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-06 09:47:20 +0530

    first token and user data  passed

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { body, validationResult, check } = require('express-validator');
+
+const User =require('../models/User')
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Privet
 router.get('/', (req, res) => {
-    res.send('Get logged in user');
+  res.send('Get logged in user');
 });
 
 // @route   POST api/auth
-// @desc    Auth user & get token
+// @desc    Auth User & get token
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Log in user');
-});
+router.post(
+    '/',
+    [
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Valid Password is required ').exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } 
+
+        const {email,password} = req.body;
+
+        try {
+          let user = await User.findOne({email});
+          if (!user){
+            return err.status(400).json({msg:'Invalid Credentails'});
+          }
+
+          const isMatched = await bcrypt.compare(password,user.password);
+
+          if(!isMatched){
+            return err.status(400).json({msg:'Wrong Password'});
+          }
+
+          const payload = {
+            user :{
+              id: user.id
+            }
+          }
+
+          jwt.sign(payload,config.get('jwtSecret'),{
+            expiresIn:360000
+          },(err,token)=>{
+            if(err) throw err;
+             res.json({token});
+             
+          });
+        } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error')
+          
+        }
+    }
+);
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
+const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const { Router } = require('express');
 const express = require('express');
+const config = require('config');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const { body, validationResult, check } = require('express-validator');
 
 const User = require('../models/User');
@@ -18,12 +22,46 @@ router.post(
             'Valid Password is required with min 6 characters'
         ).isLength({ min: 6 }),
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('passed');
+        // res.send('passed');
+        const { name, email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ msg: 'User already exists' });
+            }
+            user = new User({
+                name,
+                email,
+                password,
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            // res.send('User saved');
+             const payload = {
+               user :{
+                 id: user.id
+               }
+             }
+
+             jwt.sign(payload,config.get('jwtSecret'),{
+               expiresIn:360000
+             },(err,token)=>{
+               if(err) throw err;
+                res.json({token});
+                
+             });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     }
 );
 // res.send(req.body);
