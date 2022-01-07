f7dabdb11f39d2bc8b7ad8b67ab6d21e97ca3010
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-07 23:54:52 +0530

    react logo and front end removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
       
         <a
           className="App-link"
