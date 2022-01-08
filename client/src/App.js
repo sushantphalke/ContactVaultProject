6a8bb6a1255e13142b6337567a9fb986d987f3a8
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-08 11:43:55 +0530

    app js to navbar pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
 
-function App() {
+import Navbar from './components/layout/Navbar';
+// import { BrowserRouter, Router, Routes } from 'react-router-dom';
+import './App.css';
+const title = 'ContactVault';
+
+const App =()=> {
     return (
         <div className='App'>
-           
+          <Navbar title={title} /> 
+            <h1>ContactVault</h1>
+            <div>
+            
+            </div>
         </div>
     );
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { PropTypes } from 'react';
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function Navbar() {
-  return (
-    <div className='navbar bg-primary'>
-      <h1>
-        <i className={icon}/>{title}
-      </h1>
-      <ul>
-        <li>
-          <Link to = '/'>Home</Link>
-        </li>
-        <li>
-          <Link to = '/about'>About</Link>
-        </li>
-      </ul>
-      
-    </div>
-  );
-}
+const Navbar = (props) => {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-content'>
+                <i id='navbar-icon' className={props.icon} />
+                <Link to='/'>
+                    <div className='navbar-btn'>{props.home}</div>
+                </Link>
+                <Link to='/About'>
+                    <div className='navbar-btn'>{props.about}</div>
+                </Link>
+            </div>
+        </nav>
+    );
+};
+
+Navbar.defaultProps = {
+    home: 'Home',
+    about: 'About',
+    user: 'User',
+    icon: 'fa fa-github',
+};
+Navbar.propTypes = {
+    home: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
