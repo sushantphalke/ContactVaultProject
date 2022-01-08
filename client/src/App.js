af786e2a8955a824982a3447d21868284a7dc8b7
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-08 21:53:57 +0530

    front end working

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
-
 import Navbar from './components/layout/Navbar';
 // import { BrowserRouter, Router, Routes } from 'react-router-dom';
 import './App.css';
 const title = 'ContactVault';
 
-const App =()=> {
+const App = () => {
     return (
         <div className='App'>
-          <Navbar title={title} /> 
-            <h1>ContactVault</h1>
-            <div>
-            
-            </div>
+            <Navbar />
+            {/* <h1 className='navbar-btn'>ContactVault</h1> */}
+            <div></div>
         </div>
     );
-}
+};
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,31 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Navbar = (props) => {
+function Navbar({ title }) {
     return (
-        <nav className='navbar'>
-            <div className='navbar-content'>
-                <i id='navbar-icon' className={props.icon} />
-                <Link to='/'>
-                    <div className='navbar-btn'>{props.home}</div>
-                </Link>
-                <Link to='/About'>
-                    <div className='navbar-btn'>{props.about}</div>
-                </Link>
+        <>
+            <div className='navbar'>
+                <h1>{title}</h1>
+                <ul className='navbar-ul'>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
             </div>
-        </nav>
+        </>
     );
-};
-
+}
+Navbar.propTypes = { title: PropTypes.string };
 Navbar.defaultProps = {
     home: 'Home',
     about: 'About',
-    user: 'User',
-    icon: 'fa fa-github',
 };
-Navbar.propTypes = {
-    home: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-};
-
 export default Navbar;
