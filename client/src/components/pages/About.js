5bdbd72dd79565f48e3054bd7d1ea50d8ae64256
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-08 10:17:13 +0530

    About app  added

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h1>About ContactVault</h1>
+            <p className='my-1'>This is your personal contact manager app</p>
+            <p className='bg-dark p'>
+                <strong>Version:</strong>1.3.4.0
+            </p>
+        </div>
+    );
 }
