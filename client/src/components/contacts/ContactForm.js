6fcbf0479d5df24c9e04a2e20890b707fdf637c8
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-09 23:43:18 +0530

    contact form component

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useState } from 'react';
+import React from 'react';
 
 export const ContactForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [contact, setContac] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        type: '',
+    });
+    const { name, email, phone, type } = contact;
+    return (
+        <form>
+            <h2 className='text-primary'>Add Contact</h2>
+            <input
+                type='text'
+                placeholder='Name'
+                name='name'
+                value={name}
+                onChange={onChange}
+            />
+            <input
+                type='text'
+                placeholder='Email'
+                name='email'
+                value={email}
+                onChange={onChange}
+            />
+            <input
+                type='text'
+                placeholder='Phone'
+                name='phone'
+                value={phone}
+                onChange={onChange}
+            />
+            <h5>Contact Type
+            </h5>
+            <input
+                type='radio'
+                placeholder='Type'
+                name='type'
+                value='personal'
+                checked={type === 'personal'}
+            />Personal{'  '}
+             <input
+                type='radio'
+                placeholder='Type'
+                name='type'
+                value='Professional'
+                checked={type === 'Professional'}
+            />Professional{'  '}
+            <button className="btn btn-primary btn-block" type='submit' value="Add Contact"></button>
+        </form>
+    );
+};
