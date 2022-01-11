5490071b6781b260ab92bd694a57722f2c07c402
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-11 15:06:41 +0530

    github commands file added temproraly

---
+++ client/src/components/contacts/contactFilter.js
@@ -0,0 +1,12 @@
+import React,{useContext,useRef} from 'react';
+import ContactContext from '../../context/contact/contactContext';
+const contactFilter = () => {
+  const contactContext = useContext(ContactContext);
+  const text = useRef('')
+  return (
+    <div>
+      
+    </div>
+  )
+};
+export default contactFilter;
