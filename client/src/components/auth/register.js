21f6bd5b87808f0ea1a2bed56a7d6fbf5712c66b
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-13 00:50:52 +0530

    register page header

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,11 +1,23 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 
- const Register = () => {
-  return (
-    <div className=''>
-<div className=''></div>
-      
-    </div>
-  )
-}
+const Register = () => {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    });
+    const { name, email, password, password2 } = user;
+    return (
+    <div className='form-container'>
+      <h1 >
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form>
+        <div className="form-group">
+          
+        </div>
+      </form>
+    </div>;)
+};
 export default Register;
