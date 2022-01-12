7b620ce6cc780ee6825b68a0fdbbf7350c989e21
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-12 20:12:30 +0530

    register and login file setting up

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -1,1 +1,24 @@
 import AuhtState from "./AuthState";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LODED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+    
+} from '../types';
+export default ()=>{
+  switch (action.type) {
+    case REGISTER_SUCCESS :
+      return {
+        
+      }
+      
+   
+  
+    default:
+      return state;
+  }
+}
