690c7dcc81bb43b6c030d351503248a283c20956
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-19 23:30:51 +0530

    exam forms and sbi collect links added

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
     const { logout } = authContext;
     const { clearContacts } = contactContext;
     
-// 
+
     const onLogout = () => {
         logout();
         clearContacts();
